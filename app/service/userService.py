2e7718f8b23800390a6f04fb70b9502d7bdf6387
Aleksey Kononov <alconmail@mail.ru>
2026-04-27 05:31:46 +0300

    Created function for remarks, that UI depends on... to draw in color (success or not) days

--- app/service/userService.py
+++ app/service/userService.py
@@ -17,8 +17,10 @@ class UserService(IUserService):
     def __init__(self, logger: ILogger, userRepo: IUserRepository):
         self.logger = logger
         self.userRepo = userRepo
-        self.dayNameRE = re.compile(r"[a-z]{,3};")
+        self.dayNameRE = re.compile(r"^[a-z]{,3};")
         self.remarkRE = re.compile(r"remark:.*")
+        self.durationsRE = re.compile(r"durations:.*;")
+        self.durationsCleanRE = re.compile(r"(durations:|;|\s)*")
 
     def getUpdatedState(self, dbPath: str) -> list[str]: 
         return self.userRepo.readData(dbPath)
@@ -36,6 +38,7 @@ class UserService(IUserService):
         
         week.append(day)
         self.userRepo.writeData(dbPath, week)
+        self.logger.info("New day was created")
 
     def updateDay(self, dbPath: str, newDay: str) -> None:
         week = self.userRepo.readData(dbPath)
@@ -58,11 +61,46 @@ class UserService(IUserService):
                 return
             
     def makeRemark(self) -> None:
-        etalon = self.userRepo.readData("db/etalon.csv")
-        userWeek = self.userRepo.readData("db/userWeek.csv")
+        class path:
+            etalon = "db/etalon.csv"
+            userWeek = "db/userWeek.csv"
+
+        etalon = self.userRepo.readData(path.etalon)
+        userWeek = self.userRepo.readData(path.userWeek)
+        etalon = {self.dayNameRE.search(day).group():day for day in etalon}
+        userWeek = {self.dayNameRE.search(day).group():day for day in userWeek}
+
+        for etalonDayName, etaloneWholeDay in etalon.items():
+            if etalonDayName in userWeek:
+                etalonDurations = self.durationsRE.search(etaloneWholeDay).group()
+                userDurations = self.durationsRE.search(userWeek[etalonDayName]).group()
+                etalonDurations = self.durationsCleanRE.sub("", etalonDurations)
+                userDurations = self.durationsCleanRE.sub("", userDurations)
+                etalonDurations = [float(el) for el in etalonDurations.split(",")]
+                userDurations = [float(el) for el in userDurations.split(",")]
+
+                for ed, ud in zip(etalonDurations, userDurations):
+                    if ud < ed and bool(self.remarkRE.search(userWeek[etalonDayName])) is True:
+                        userWeek[etalonDayName] = self.remarkRE.sub("remark:red", userWeek[etalonDayName])
+                        break
+                    elif ud < ed:
+                        userWeek[etalonDayName] += ";remark:red"
+                        break
+                    
+                if bool(self.remarkRE.search(userWeek[etalonDayName])) is False:
+                        userWeek[etalonDayName] += ";remark:green"
+                
+        etalon = [day for _,day in etalon.items()]
+        userWeek = [day for _,day in userWeek.items()]
+        self.userRepo.writeData(path.etalon, etalon)
+        self.userRepo.writeData(path.userWeek, userWeek)
+                
+                
 
 
-        for et, uw in zip(etalon, userWeek):  
-            pass 
+
+
+
+
         
             
